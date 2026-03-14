36a7629fa9c0b9cddb9ee655eea2bb461546bb6a
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-03-14 14:59:10 -0300

    Customizing challenge list

---
+++ src/screens/home/components/ChallengeList/components/ChallengeCard/index.tsx
@@ -0,0 +1,67 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { ArrowRight } from "lucide-react-native";
+import { Pressable, StyleSheet, View } from "react-native";
+import Animated from "react-native-reanimated";
+
+import { usePressAnimation } from "@/animations/hooks/usePressAnimation";
+import { colors } from "@/constants/colors";
+import { AppText } from "@/shared/components/AppText";
+import { ChallengeTheme } from "@/shared/utils/challenge";
+
+interface ChallengeCardProps {
+  challenge: ChallengeTheme;
+}
+
+export function ChallengeCard({ challenge }: Readonly<ChallengeCardProps>) {
+  const pressAnimation = usePressAnimation();
+
+  return (
+    <LinearGradient
+      colors={challenge.gradient as readonly [string, string, ...string[]]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+      style={styles.container}
+    >
+      <Animated.View style={pressAnimation.animatedStyle}>
+        <Pressable style={styles.content}>
+          <AppText style={styles.title}>{challenge.title}</AppText>
+          <View
+            style={[
+              styles.arrowButton,
+              { backgroundColor: challenge.arrowColor },
+            ]}
+          >
+            <ArrowRight size={20} color={colors.grayscale.gray700} />
+          </View>
+        </Pressable>
+      </Animated.View>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 16,
+    overflow: "hidden",
+    borderWidth: 1,
+  },
+  content: {
+    padding: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    maxWidth: "50%",
+    fontSize: 18,
+    fontFamily: "Baloo2_800ExtraBold",
+    color: colors.grayscale.gray100,
+  },
+  arrowButton: {
+    width: 40,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "50%",
+  },
+});
--- src/screens/home/components/ChallengeList/index.tsx
+++ src/screens/home/components/ChallengeList/index.tsx
@@ -1,17 +1,30 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
+import { colors } from "@/constants/colors";
 import { AppText } from "@/shared/components/AppText";
 import { challengeTheme } from "@/shared/utils/challenge";
+import { ChallengeCard } from "./components/ChallengeCard";
 
 export function ChallengeList() {
   return (
-    <View>
-      <AppText>Desafios diponíveis</AppText>
-      {challengeTheme.map(({ id, title }) => (
-        <View key={id}>
-          <AppText>{title}</AppText>
-        </View>
+    <View style={styles.container}>
+      <AppText style={styles.title}>Desafios diponíveis</AppText>
+      {challengeTheme.map((challenge) => (
+        <ChallengeCard
+          key={`challenge-id-${challenge.id}`}
+          challenge={challenge}
+        />
       ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 12,
+  },
+  title: {
+    fontSize: 16,
+    color: colors.grayscale.gray200,
+  },
+});
